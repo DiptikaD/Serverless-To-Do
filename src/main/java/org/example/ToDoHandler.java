fbf7c04159c3ef2455465d92f2ac38cd6558af58
Devi <diptikadevi@gmail.com>
2024-10-09 12:43:57 -0400

    create todo method creation

--- src/main/java/org/example/ToDoHandler.java
+++ src/main/java/org/example/ToDoHandler.java
@@ -2,10 +2,14 @@ package org.example;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ToDoHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     private final DynamoDB dynamoBD = new DynamoDB(AmazonDynamoDBAsyncClientBuilder.defaultClient());
@@ -27,6 +31,9 @@ public class ToDoHandler implements RequestHandler<APIGatewayProxyRequestEvent, 
                     responseBody = getToDo();
                     break;
                 case "PUT":
+//                    responseBody = updateToDo();
+//                case "DELETE":
+//                    responseBody = deleteToDo();
                 default:
                     statusCode = 400;
                     responseBody = "Unsupported Method :(";
@@ -41,9 +48,26 @@ public class ToDoHandler implements RequestHandler<APIGatewayProxyRequestEvent, 
                 .withBody(responseBody);
     }
 
-    private String getToDo() {
+    private String createToDo(APIGatewayProxyRequestEvent request) throws JsonProcessingException {
+    String body = request.getBody();
+    ToDo todo = new ObjectMapper().readValue(body, ToDo.class);
+    String id = java.util.UUID.randomUUID().toString();
+
+    Item item = new Item()
+            .withPrimaryKey("id", id)
+            .withString("title", todo.getTitle())
+            .withString("description", todo.getDescription())
+            .withBoolean("completed", false);
+
+    Table table = dynamoBD.getTable(tableName);
+    table.putItem(item);
+    return id;
     }
 
-    private String createToDo(APIGatewayProxyRequestEvent request) {
+
+    private String getToDo() {
+        return "List of ToDos";
     }
+
+    //other fetching operations
 }
