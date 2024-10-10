5b1f4b7c52144a8f5bb2b42cad317942b180bb06
Devi <diptikadevi@gmail.com>
2024-10-10 11:20:28 -0400

    get all todo fetch for list of tasks

--- src/main/java/org/example/ToDoHandler.java
+++ src/main/java/org/example/ToDoHandler.java
@@ -4,6 +4,9 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
@@ -11,8 +14,12 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class ToDoHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
-    private final DynamoDB dynamoBD = new DynamoDB(AmazonDynamoDBAsyncClientBuilder.defaultClient());
+    private final DynamoDB dynamoDB = new DynamoDB(AmazonDynamoDBAsyncClientBuilder.defaultClient());
     private final String tableName = "Todo";
 
 
@@ -28,7 +35,7 @@ public class ToDoHandler implements RequestHandler<APIGatewayProxyRequestEvent, 
                     responseBody = createToDo(request);
                     break;
                 case "GET":
-                    responseBody = getToDo();
+                    responseBody = getAllToDos();
                     break;
                 case "PUT":
 //                    responseBody = updateToDo();
@@ -59,14 +66,36 @@ public class ToDoHandler implements RequestHandler<APIGatewayProxyRequestEvent, 
             .withString("description", todo.getDescription())
             .withBoolean("completed", false);
 
-    Table table = dynamoBD.getTable(tableName);
+    Table table = dynamoDB.getTable(tableName);
     table.putItem(item);
     return id;
     }
 
 
-    private String getToDo() {
-        return "List of ToDos";
+    private String getAllToDos() {
+        List<ToDo> toDoList = new ArrayList<>();
+
+        Table table = dynamoDB.getTable(tableName);
+
+        ScanRequest scanRequest = new ScanRequest().withTableName(tableName);
+
+        ScanResult result = AmazonDynamoDBAsyncClientBuilder.defaultClient().scan(scanRequest);
+
+        for (Map<String, AttributeValue> item : result.getItems()){
+            ToDo toDo = new ToDo();
+            toDo.changeTitle(item.get("title").getS());
+            toDo.changeDescription(item.get("description").getS());
+            toDo.getCompletionStatus();
+
+            toDoList.add(toDo);
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writeValueAsString(toDoList);
+        } catch (JsonProcessingException e){
+            e.printStackTrace();
+        }return "Error converting to dos into JSON format!";
     }
 
     //other fetching operations
