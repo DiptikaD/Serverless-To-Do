4f55e4cd9d9d7122400152e52efc935d1f23c4e8
Devi <diptikadevi@gmail.com>
2024-10-10 11:02:58 -0400

    todo class constructors

--- src/main/java/org/example/ToDo.java
+++ src/main/java/org/example/ToDo.java
@@ -1,4 +1,21 @@
 package org.example;
 
 public class ToDo {
+
+    private String title, description;
+    private boolean completed;
+
+    public ToDo(){
+        title = "untitiled";
+        description = "no description";
+        completed = false;
+    }
+
+    public ToDo (String inputTitle, String inputDescription, Boolean inputCompleted){
+        title = inputTitle;
+        description = inputDescription;
+        completed = inputCompleted;
+    }
+
+
 }
