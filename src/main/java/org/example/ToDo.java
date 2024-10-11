d4efc812487fba55a19c534a3d78a8c5d1a92f7d
Devi <diptikadevi@gmail.com>
2024-10-11 10:37:32 -0400

    refactoring code for spring framework

--- src/main/java/org/example/ToDo.java
+++ src/main/java/org/example/ToDo.java
@@ -1,47 +1,16 @@
 package org.example;
 
+import javax.persistence.Id;
+import lombok.Data;
+
 public class ToDo {
 
-    private String title, description;
-    private boolean completed;
-
-    public ToDo(){
-        title = "untitiled";
-        description = "no description";
-        completed = false;
-    }
-
-    public ToDo (String inputTitle, String inputDescription, Boolean inputCompleted){
-        title = inputTitle;
-        description = inputDescription;
-        completed = inputCompleted;
-    }
-
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Boolean getCompletionStatus(){
-        return completed;
-    }
-
-    public Boolean changeCompleted(){
-        completed = !completed;
-        return completed;
-    }
-
-    public void changeTitle(String newTitle){
-        title = newTitle;
-    }
-
-    public void changeDescription(String newDescription){
-        description = newDescription;
-    }
-
-    //write tests
+   @Data
+    public class Todo {
+       @Id
+       private String id;
+       private String title;
+       private String description;
+       private boolean completed;
+   }
 }
