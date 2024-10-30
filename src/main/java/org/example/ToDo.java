24c8a1d4a223ad3c6d42a2e5bf867c8546033516
Devi <diptikadevi@gmail.com>
2024-10-30 14:18:38 -0400

    todo class adjust title to task

--- src/main/java/org/example/ToDo.java
+++ src/main/java/org/example/ToDo.java
@@ -1,7 +1,4 @@
 package org.example;
-
-import javax.persistence.Id;
-
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 import lombok.Data;
 
@@ -11,15 +8,14 @@ import java.util.Map;
 @Data
 public class ToDo {
 
-       @Id
        private String id;
-       private String title;
+       private String task;
        private String description;
        private boolean completed;
 
-       public ToDo(String id, String title, String description, boolean completed) {
+       public ToDo(String id, String task, String description, boolean completed) {
               this.id = id;
-              this.title = title;
+              this.task = task;
               this.description = description;
               this.completed = completed;
        }
@@ -28,7 +24,7 @@ public class ToDo {
               Map<String, AttributeValue> attributeValueMap = new HashMap<>();
 
               attributeValueMap.put("id", AttributeValue.builder().s(id).build());
-              attributeValueMap.put("title", AttributeValue.builder().s(title).build());
+              attributeValueMap.put("task", AttributeValue.builder().s(task).build());
               attributeValueMap.put("description", AttributeValue.builder().s(description).build());
               attributeValueMap.put("completed",AttributeValue.builder().bool(completed).build());
 
