11a922be9508467f5969891e76ef53715d6ab7a7
Devi <diptikadevi@gmail.com>
2024-11-07 10:50:19 -0500

    dynamodb config bean for region

--- src/main/java/org/example/DynamoDBConfig.java
+++ src/main/java/org/example/DynamoDBConfig.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
@@ -9,6 +10,8 @@ public class DynamoDBConfig {
 
     @Bean
     public DynamoDbClient dynamoDBClient(){
-        return DynamoDbClient.builder().build();
+        return DynamoDbClient.builder()
+                .region(Region.of("us-east-1"))
+                .build();
     }
 }
