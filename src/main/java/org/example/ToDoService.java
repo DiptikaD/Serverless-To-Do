1111448b9771209f6df28ab63ac142ba4f58fd07
Devi <diptikadevi@gmail.com>
2024-11-12 13:08:43 -0500

    added constructor, adjusted task parameter for range key

--- src/main/java/org/example/ToDoService.java
+++ src/main/java/org/example/ToDoService.java
@@ -2,25 +2,29 @@ package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ToDoService {
 
-    @Autowired
     private ToDoRepository toDoRepository;
+
+    @Autowired
+    public ToDoService (ToDoRepository inputtedRepository){
+        toDoRepository = inputtedRepository;
+    }
 
     public ToDo createToDo(ToDo todo){
         todo.setId(java.util.UUID.randomUUID().toString());
         return toDoRepository.save(todo);
     }
 
-    public Optional<ToDo> getToDoByID(String id){
-        return toDoRepository.findById(id);
+    public Optional<ToDo> getToDoByIDandTask(String id, String task){
+        return toDoRepository.findByIdandTask(id, task);
     }
 
-    public Iterable<ToDo> getAllToDo(){
+    public List<ToDo> getAllToDo(){
         return toDoRepository.findAll();
     }
 
@@ -28,8 +32,8 @@ public class ToDoService {
         return toDoRepository.save(todo);
     }
 
-    public void deleteToDoByID(String id){
-        toDoRepository.deleteById(id);
+    public void deleteToDoByIDandTask(String id, String task){
+        toDoRepository.deleteByIdandTask(id, task);
     }
 
 }
